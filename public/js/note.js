b725ebcb956350d20f15f2484ebac2041dc7ff53
bbarksse <bbarksse@gmail.com>
2018-12-31 11:02:26 +0900

    첫번째 커밋

---
+++ public/js/note.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB8xOq7MHOCQmI5AZE3t6RETVYggcgLbvg",
+    authDomain: "bbarksse-note.firebaseapp.com",
+    databaseURL: "https://bbarksse-note.firebaseio.com",
+    projectId: "bbarksse-note",
+    storageBucket: "bbarksse-note.appspot.com",
+    messagingSenderId: "988186085837"
+  };
+  firebase.initializeApp(config);
